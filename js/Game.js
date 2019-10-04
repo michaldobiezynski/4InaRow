b9ad370500ff03b795d10c1e03b0b251884484f7
dobimi <michal.dobiezynski@woodmac.com>
2019-10-04 10:28:24 +0100

    created the game constructor

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,20 @@
 class Game {
+    constructor() {
+        this.players = this.createPlayers();
+        this.board = new Board();
+        this.ready = false;
+    }
+
+    createPlayers() {
+        const players =[];
+
+        const players1 = new Player("Player 1", 1, "#e15258", true);
+        const players2 = new Player("Player 2", 2, "#e59a13");
+
+        players.push(players1);
+        players.push(players2);
+
+        return players;
+    }
 
 }

036e4f2d5884366cd0c80776d0d960478fb7d4d9
dobimi <michal.dobiezynski@woodmac.com>
2019-10-04 11:51:41 +0100

    created svg space

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const beginGame = document.getElementById('begin-game');
 
 beginGame.addEventListener('click', function() {
     game.startGame();
-    
+
     this.style.display = 'none';
     document.getElementById('play-area').style.opacity = '1';
 });

cd4b481834001d1961c052c41c7f196d32f33ff7
dobimi <michal.dobiezynski@woodmac.com>
2019-10-04 10:14:38 +0100

    created method in board constructor to create all the spaces on the board using a space constructors

--- js/Board.js
+++ js/Board.js
@@ -2,7 +2,23 @@ class Board {
     constructor() {
         this.rows = 6;
         this.columns = 7;
-        this.spaces = [];
+        this.spaces = createSpaces();
+    }
+
+    createSpaces() {
+        const spaces =[];
+
+        for(let i =0; i<this.columns; i++) {
+            const column =[];
+
+            for(let j =0; j<this.rows; j++) {
+                const space = new Space(i,j);
+                column.push(space);
+            }
+            spaces.push(column)
+        }
+
+        return spaces;
     }
 
 }

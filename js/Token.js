39b4339cc11de1d2544e2aee00147221adef9b39
dobimi <michal.dobiezynski@woodmac.com>
2019-10-04 13:52:55 +0100

    created a method to draw a html token and created a getter for it

--- js/Token.js
+++ js/Token.js
@@ -4,4 +4,21 @@ class Token {
         this.id = `token-${index}-${owner.id}`;
         this.dropped = false;
     }
+
+    drawHTMLToken() {
+
+        const divEle =  document.createElement('<div></div>');
+        divEle('<div></div>').setAttribute("id", this.id);
+        divEle('<div></div>').setAttribute("class", "token");
+        divEle.style.background = divEle.ownerElement.style.background;
+
+        document.getElementById('game-board-underlay')
+            .append(divEle);
+
+    }
+
+    get htmlToken() {
+        return document.getElementById(this.id);
+    }
+
 }

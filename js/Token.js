79c8da09d8fd77dce9b67ed88d25c0da565cf6de
dobimi <michal.dobiezynski@woodmac.com>
2019-10-18 15:02:31 +0100

    methods for moving left and right, and for dropping the token

--- js/Token.js
+++ js/Token.js
@@ -3,6 +3,7 @@ class Token {
         this.owner = owner;
         this.id = `token-${index}-${owner.id}`;
         this.dropped = false;
+        this.columnLocation = 0;
     }
 
     drawHTMLToken() {
@@ -17,6 +18,32 @@ class Token {
 
     get htmlToken() {
         return document.getElementById(this.id);
+    }
+
+    get offsetLeft() {
+        return this.htmlToken.offsetLeft;
+    }
+
+    moveLeft() {
+        if(this.columnLocation > 0) {
+            this.htmlToken.style.left = this.offsetLeft - 76;
+            this.columnLocation -=1;
+        }
+    }
+    moveRight(columns) {
+        if(this.columnLocation < columns - 1) {
+            this.htmlToken.style.left = this.offsetLeft + 76;
+            this.columnLocation +=1;
+        }
+    }
+
+    drop(target, reset) {
+
+    this.dropped = true;
+    
+        $(this.htmlToken).animate({
+            top: (target.y * target.diameter)
+        }, 750, 'easeOutBounce', reset);
     }
 
 }

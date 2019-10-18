25a2068f56a23126fa0ac012fc529698e84d57ff
dobimi <michal.dobiezynski@woodmac.com>
2019-10-18 15:21:28 +0100

    added a method call in the game class to play the token

--- js/Game.js
+++ js/Game.js
@@ -26,13 +26,13 @@ class Game {
     }
 
     handleKeydown(e) {
-        if(this.ready) {
-            if(e.key === "ArrowLeft") {
-               this.activePlayer.activeToken.moveLeft();
-            } else if(e.key === "ArrowRight") {
+        if (this.ready) {
+            if (e.key === "ArrowLeft") {
+                this.activePlayer.activeToken.moveLeft();
+            } else if (e.key === "ArrowRight") {
                 this.activePlayer.activeToken.moveRight(this.board.columns);
-            } else if(e.key === "ArrowDown") {
-                //play token
+            } else if (e.key === "ArrowDown") {
+                this.playToken();
             }
         }
     }

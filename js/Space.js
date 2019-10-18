ddaa8f4a572698335e9397f8fbf490c7fd1650fe
dobimi <michal.dobiezynski@woodmac.com>
2019-10-18 15:29:38 +0100

    refactored space class

--- js/Space.js
+++ js/Space.js
@@ -2,15 +2,17 @@ class Space {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.id = `spaces-${x}-${y}`;
+        this.id = `space-${x}-${y}`;
         this.token = null;
         this.diameter = 76;
         this.radius = this.diameter/2;
     }
 
+    /**
+     * Draws SVG space
+     */
     drawSVGSpace() {
         const svgSpace = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-
         svgSpace.setAttributeNS(null, "id", this.id);
         svgSpace.setAttributeNS(null, "cx", (this.x * this.diameter) + this.radius);
         svgSpace.setAttributeNS(null, "cy", (this.y * this.diameter) + this.radius);
@@ -20,5 +22,4 @@ class Space {
 
         document.getElementById("mask").appendChild(svgSpace);
     }
-
 }
